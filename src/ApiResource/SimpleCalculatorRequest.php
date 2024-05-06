7c7ca5f78ecd10041c42879da8e684534b9aa983
JPechberty <j.pechberty@gmail.com>
2024-05-06 16:49:15 +0200

    Add yaml config for SimpleCalculatorRequest

--- src/ApiResource/SimpleCalculatorRequest.php
+++ src/ApiResource/SimpleCalculatorRequest.php
@@ -11,32 +11,10 @@ use App\State\SimpleCalculatorRequestProcessor;
 use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
-#[ApiResource(
-    operations: [
-        new Post(
-            uriTemplate: "/calculate/simple",
-            openapi: new Model\Operation(
-                summary: 'Calculate with 2 operands',
-                description: 'This operation allows you to calculate with 2 operands',
-            ),
-            //controller: SimpleCalculatorRequestAction::class,
-            normalizationContext: ['groups' => ['simple_calculator_request:read']],
-            denormalizationContext: ['groups' => ['simple_calculator_request:write']],
-            input: SimpleCalculatorRequest::class,
-            output: SimpleCalculatorRequest::class,
-            processor: SimpleCalculatorRequestProcessor::class
-        )
-    ]
-)]
+
 class SimpleCalculatorRequest
 {
-    #[ApiProperty(
-        openapiContext: [
-            'type' => 'enum',
-            'enum' => ['add', 'subtract', 'multiply', 'divide']
-        ]
-    )]
-    #[Groups([ 'simple_calculator_request:write'])]
+
     #[Assert\Choice(choices: ['add', 'subtract', 'multiply', 'divide'])]
     public string $operation;
     #[ApiProperty(
